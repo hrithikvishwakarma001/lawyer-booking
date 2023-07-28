607e43b731bcd9f843cf40ae8c4c07044a7c5d4d
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-07-28 14:40:06 +0530

    add react tostify

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Navbar } from "./components";
 import { Home } from "./pages";
 import { Container } from "@mui/material";
-
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 function App() {
 	return (
 		<Container fixed>
 			<Navbar />
 			<Home />
+			<ToastContainer />
 		</Container>
 	);
 }
--- src/redux/lawyersReducer/action.js
+++ src/redux/lawyersReducer/action.js
@@ -1,6 +1,7 @@
 import * as Types from "./actionTypes";
 import axios from "axios";
 import { JSON_SERVER_URL } from "../../utils";
+import { toast } from "react-toastify";
 
 const LawyerAPI = axios.create({
 	baseURL: `${JSON_SERVER_URL}/lawyers`,
@@ -28,13 +29,16 @@ export const filterLawyers = (searchTerm) => async (dispatch) => {
 	}
 };
 
-export const EditLawyerBookingStatus = (id, data) => async (dispatch) => {
+export const EditLawyerBookingStatus = (id) => async (dispatch) => {
 	try {
 		let res = await LawyerAPI.patch(`/${id}`, {
 			isBooked: true,
 		});
 		if (res.status === 200) {
 			dispatch(getLawyers());
+			toast.success("Booking Successful");
+		} else {
+			toast.error("Booking Failed");
 		}
 	} catch (error) {
 		dispatch({ type: Types.FETCH_LAWYERS_FAILURE, payload: error });
